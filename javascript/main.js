0304878f6bcadebfbcb04f9cf0ea9a04908c7916
Vyrrhus <baptistedesoubrie@gmail.com>
2017-12-29 22:12:23 +0100

    Update main.js

--- javascript/main.js
+++ javascript/main.js
@@ -1,6 +1,7 @@
 // Canvas
+var DRAWING = document.getElementById('drawing');
 var BACKGROUND = document.getElementById('background');
-var DRAWING = document.getElementById('canvas');
+
 
 // Contexts
 var context_BACKGROUND = BACKGROUND.getContext('2d');
@@ -15,35 +16,34 @@ var HEIGHT = document.getElementById('body').offsetHeight;
 var CENTER = [Math.floor(WIDTH/2), Math.floor(HEIGHT/2)];
 
 /* Adjust the size of canvas to the size of the screen */
-//window.onload = function() {
-//	function resize() {
-//		WIDTH = document.getElementById('body').offsetWidth;
-//		HEIGHT = document.getElementById('body').offsetHeight;
-//		CENTER = [Math.floor(WIDTH/2), Math.floor(HEIGHT/2)];
-//		resizeCanvas(BACKGROUND, DRAWING, INTERFACE);
-//		function resizeCanvas() {
-//			[].forEach.call(arguments, function (element) {
-//				element.width = WIDTH;
-//				element.height = HEIGHT;
-//			});
-//		}
-//	}
-//	resize();
-//	context_DRAWING.fillStyle = "#FF0000"; 
-//	context_DRAWING.fillRect(CENTER[0],CENTER[1],150,75);
-//	window.addEventListener('resize', function() {
-//		console.log('resize');
-//		console.log(CENTER);
-//		resize();
-//		console.log(CENTER);
-//	}, false);
-//}
-//
-//// Settings
+window.onload = function() {
+	function resize() {
+		WIDTH = document.getElementById('body').offsetWidth;
+		HEIGHT = document.getElementById('body').offsetHeight;
+		CENTER = [Math.floor(WIDTH/2), Math.floor(HEIGHT/2)];
+		resizeCanvas(BACKGROUND, DRAWING);
+		function resizeCanvas() {
+			[].forEach.call(arguments, function (element) {
+				element.width = WIDTH;
+				element.height = HEIGHT;
+			});
+		}
+		background('black', context_BACKGROUND);
+	}
+	resize();
+	window.addEventListener('resize', function() {
+		console.log('resize');
+		resize();
+	}, false);
+}
 
 // Background
-function background(X,Y,COLOR,CONTEXT) {
-	
+function background(COLOR,CONTEXT) {
+	CONTEXT.beginPath();
+	CONTEXT.rect(0,0,WIDTH,HEIGHT);
+	CONTEXT.fillStyle = COLOR;
+	CONTEXT.fill();
+	CONTEXT.closePath();
 }
 
 // Circles defined
@@ -79,13 +79,15 @@ var c0 = circle(CENTER[0]+200, CENTER[1], 50, 'green', context_DRAWING);
 var c1 = circle(c0.x + c0.radius, CENTER[1], 30, 'blue', context_DRAWING);
 var c2 = circle(c1.x + c1.radius, CENTER[1], 20, 'red', context_DRAWING);
 var c3 = circle(c2.x + c2.radius, CENTER[1], 10, 'yellow', context_DRAWING);
-var c4 = circle(c3.x + c3.radius, CENTER[1], 5, 'black', context_DRAWING);
+var c4 = circle(c3.x + c3.radius, CENTER[1], 5, 'white', context_DRAWING);
 
+background('black', context_BACKGROUND);
 c0.draw();
 c1.draw();
 c2.draw();
 c3.draw();
 c4.draw();
+
 
 //Drawing
 angle = 0;
@@ -93,11 +95,11 @@ velocity = 0.38;
 function draw() {
 	context_DRAWING.clearRect(0, 0, WIDTH, HEIGHT);
 	c0.draw();
+	c0.line();
 	c1.draw();
 	c2.draw();
 	c3.draw();
 	c4.draw();
-	c0.line();
 	c4.line();
 	angle = (angle+Math.PI/720 * velocity)%(4*Math.PI);
 	
