ccdc59b4a9046ca70fd9b662f80ce0d9bd4c93c3
Vyrrhus <baptistedesoubrie@gmail.com>
2017-12-31 18:42:12 +0000

    Update data.js

--- javascript/data.js
+++ javascript/data.js
@@ -5,7 +5,7 @@ var SUN = new _planet('SUN',
 				  'yellow',
 				  null);
 
-// Rank 1
+// Orbit Rank 1
 var ORBIT_MERCURY 	= new _orbit(SUN, 
 						   	7.003938621978453, 
 						   	4.830840964417403E+01, 
@@ -35,23 +35,78 @@ var ORBIT_MARS 		= new _orbit(SUN,
 							 2.866026779027202E+02,	
 							 2.176767558580171E+02);
 
-var MERCURY = new _planet('MERCURY',
-					  0.330104e24,
-					  2439.7,
-					  'orange',
-					  ORBIT_MERCURY);
-var VENUS = new _planet('VENUS', 
-				    4.86732e24,
-				    6051.8,
-				    'green',
-				    ORBIT_VENUS);
-var EARTH = new _planet('EARTH',
-				    5.97219e24,
-				    6371.009,
-				    'blue',
-				    ORBIT_EARTH);
-var MARS = new _planet('MARS',
-				   0.641693e24,
-				   3389.5,
-				   'red',
-				   ORBIT_MARS);
+var ORBIT_JUPITER	= new _orbit(SUN,
+							1.303637966856463E+00, 
+							1.005213157189207E+02, 
+							5.202542422947007E+00, 
+							4.885927439345982E-02, 
+							2.736042883561856E+02, 
+							2.041846436041122E+02);
+
+var ORBIT_SATURNE	= new _orbit(SUN,
+							2.487689401889006E+00, 
+							1.136052151676273E+02, 
+							9.572140096816266E+00, 
+							5.155881895496966E-02, 
+							3.399985499049498E+02, 
+							1.766618534596307E+02);
+
+var ORBIT_URANUS	= new _orbit(SUN,
+							7.700798753715145E-01,
+							7.412911928059437E+01,
+							1.913232112250847E+01,
+							4.929510554854896E-02,
+							9.930241211616277E+01,
+							2.136359804981328E+02);
+
+var ORBIT_NEPTUNE	= new _orbit(SUN,
+							1.777597703201860E+00, 
+							1.319250489390328E+02,
+							3.004852847210088E+01,
+							6.477639267230120E-03,
+							2.694963452560274E+02,
+							3.019029128867259E+02);
+
+
+// Planets Rank 1
+var MERCURY = new _planet(	'MERCURY',
+					  		0.330104e24,
+					  		2439.7,
+					  		'#929292',
+					  		ORBIT_MERCURY);
+var VENUS 	= new _planet(	'VENUS', 
+				    		4.86732e24,
+				    		6051.8,
+				    		'#dabeac',
+				    		ORBIT_VENUS);
+var EARTH 	= new _planet(	'EARTH',
+				    		5.97219e24,
+				    		6371.009,
+				    		'#5880b8',
+				    		ORBIT_EARTH);
+var MARS 	= new _planet(	'MARS',
+				   			0.641693e24,
+				   			3389.5,
+				   			'#d8b059',
+				   			ORBIT_MARS);
+
+var JUPITER = new _planet(	'JUPITER',
+				   			1898.13e24,
+				   			69911,
+				   			'#a98723',
+				   			ORBIT_JUPITER);
+var SATURNE = new _planet(	'SATURNE',
+				   			568.319e24,
+				  		 	58232,
+				   			'#f7e4c8',
+				   			ORBIT_SATURNE);
+var URANUS 	= new _planet(	'URANUS',
+				   			86.8103e24,
+				   			25362,
+				   			'#b0b2c8',
+				   			ORBIT_URANUS);
+var NEPTUNE = new _planet(	'NEPTUNE',
+				   			102.410e24,
+				   			24622,
+				   			'#004e73',
+				   			ORBIT_NEPTUNE);
