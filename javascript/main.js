6038b3ea5110dfb5607506e3b93b2e33fa8abf4a
Vyrrhus <baptistedesoubrie@gmail.com>
2018-01-02 00:21:35 +0000

    Synthetize functions

--- javascript/main.js
+++ javascript/main.js
@@ -22,11 +22,6 @@ window.onload = function() {
 	}
 }
 
-
-		context_TEXT.font = "16px Arial";
-		context_TEXT.fillStyle = "#BBB";
-		context_TEXT.fillText("Time: " + this.date, 10, 30);
-
 // Fill canvas with 'background.png'
 function set_BACKGROUND() {
 	var img_BACKGROUND = new Image();
@@ -50,38 +45,31 @@ function set_BACKGROUND() {
 	img_BACKGROUND.src = 'img/background.png';
 }
 
-var clog = true;
+function planet_animation() {
+	[].forEach.call(arguments, function(element) {
+			element.orbit.motion(TIME.dT);
+	});
+}
+function planet_draw() {
+	[].forEach.call(arguments, function(element) {
+			element.draw();
+	});
+}
+
 FOCUS.change(SUN);
 
 function draw() {
 	context_ANIMATION.clearRect(0,0,WIDTH,HEIGHT);
-	MERCURY.orbit.motion(TIME.dT);
-	VENUS.orbit.motion(TIME.dT);
-	EARTH.orbit.motion(TIME.dT);
-	MARS.orbit.motion(TIME.dT);
-	JUPITER.orbit.motion(TIME.dT);
-	SATURNE.orbit.motion(TIME.dT);
-	URANUS.orbit.motion(TIME.dT);
-	NEPTUNE.orbit.motion(TIME.dT);
-	MOON.orbit.motion(TIME.dT);
+	planet_animation(MERCURY, VENUS, EARTH, MARS, JUPITER, SATURNE, URANUS, NEPTUNE, PLUTO, MOON);
 	TIME.value += TIME.dT;
 	TIME.draw();
-	
 	FOCUS.planet.setFocus();
+	planet_draw(SUN, MERCURY, VENUS, EARTH, MARS, JUPITER, SATURNE, URANUS, NEPTUNE, PLUTO, MOON);
 	
-	SUN.draw();
-	MERCURY.draw();
-	VENUS.draw();
-	EARTH.draw();
-	MARS.draw();
-	JUPITER.draw();
-	URANUS.draw();
-	SATURNE.draw();
-	NEPTUNE.draw();
-	MOON.draw();
 	if (RUNNING) {
 		requestAnimationFrame(draw);
 	}
 }
+
 draw();
 
