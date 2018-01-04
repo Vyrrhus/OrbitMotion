6d893394f2c57509af54681ca6e58ffd7ac48004
Vyrrhus <baptistedesoubrie@gmail.com>
2018-01-04 04:28:44 +0000

    Mousedown & mouseup events

--- javascript/event.js
+++ javascript/event.js
@@ -15,14 +15,14 @@ document.addEventListener('keypress', function(event) {
 	if (event.key === '4') {FOCUS.change(EARTH);}
 	if (event.key === '5') {FOCUS.change(MARS);}
 	if (event.key === '6') {FOCUS.change(JUPITER);}
-	if (event.key === '7') {FOCUS.change(SATURNE);}
+	if (event.key === '7') {FOCUS.change(SATURN);}
 	if (event.key === '8') {FOCUS.change(URANUS);}
 	if (event.key === '9') {FOCUS.change(NEPTUNE);}
 	if (event.key === 'a') {
 		BUTTON.draw_ORBIT.switch();
 	}
 	if (event.keyCode === 32 || event.key === 'q') {
-		BUTTON.draw_TIMELINE.switch();
+		BUTTON.draw_TIMELINE.play_pause.switch();
 	}
 	if (event.keyCode === 47) {TIME.dT *= -1;}
 	if (event.key === '0') {
@@ -38,7 +38,7 @@ document.addEventListener('keypress', function(event) {
 
 document.addEventListener('wheel', function(event) {
 	ZOOM.num += event.deltaY/100;
-	if (ZOOM.num < 0 || ZOOM.num > 85) {
+	if (ZOOM.num < 0 || ZOOM.num > 92) {
 		ZOOM.num -= event.deltaY/100;
 	}
 	if (ZOOM.num < 55) {ZOOM.unit = 1;ZOOM.unit_name = ' km';}
@@ -61,6 +61,21 @@ document.addEventListener('click', function(event) {
 	// Button draw orbit
 	if (button_frame(15,70,15,20)) {BUTTON.draw_ORBIT.switch();}
 	
+	// Button draw informations
+	if (button_frame(15,95,15,20)) {BUTTON.draw_INFO.switch();}
+	if (button_frame(35,115,15,20)) {
+		BUTTON.draw_INFO.characteristics = !BUTTON.draw_INFO.characteristics;
+		BUTTON.draw_INFO.draw();
+	}
+	if (button_frame(35,135,15,20)) {
+		BUTTON.draw_INFO.elements = !BUTTON.draw_INFO.elements;
+		BUTTON.draw_INFO.draw();
+	}
+	if (button_frame(35,155,15,20)) {
+		BUTTON.draw_INFO.vector = !BUTTON.draw_INFO.vector;
+		BUTTON.draw_INFO.draw();
+	}
+	
 	// Legend for each planet
 	check_coordinates(DATA);
 	function check_coordinates() {
@@ -70,29 +85,90 @@ document.addEventListener('click', function(event) {
 			}
 		});
 	}
+});
+
+document.addEventListener('mousedown', function(event) {
+	let mouseX = event.clientX;
+	let mouseY = event.clientY;
+	function button_frame(x0,y0,dx,dy) {
+		if (mouseX >= x0 && mouseX <= (x0+dx) && mouseY >= y0 && mouseY <= (y0+dy)) {return true;}
+		else {return false;}
+	}
 	
-	// Timeline buttons
-	if (button_frame(WIDTH/2-160,HEIGHT-100,80,80)) {TIME.dT*=0.8;}
-	if (button_frame(WIDTH/2-80,HEIGHT-100,80,80)) {BUTTON.draw_TIMELINE.switch();}
-	if (button_frame(WIDTH/2,HEIGHT-100,80,80)) {
+	// Rewind
+	if (button_frame(BUTTON.draw_TIMELINE.rewind.X,BUTTON.draw_TIMELINE.rewind.Y,60,60)) {
+		BUTTON.draw_TIMELINE.rewind.state = true;
+		BUTTON.draw_TIMELINE.rewind.draw();
+	}
+	
+	// Play && Pause
+	if (button_frame(BUTTON.draw_TIMELINE.play_pause.X, BUTTON.draw_TIMELINE.play_pause.Y, 60, 60)) {
+			BUTTON.draw_TIMELINE.play_pause.state = true;
+			BUTTON.draw_TIMELINE.play_pause.draw();
+		}
+	
+	// Stop
+	if (button_frame(BUTTON.draw_TIMELINE.stop.X,BUTTON.draw_TIMELINE.stop.Y,60,60)) {
+		BUTTON.draw_TIMELINE.stop.state = true;
+		BUTTON.draw_TIMELINE.stop.draw();
+	}
+	
+	// Fast forward
+	if (button_frame(BUTTON.draw_TIMELINE.fast_forward.X,BUTTON.draw_TIMELINE.fast_forward.Y,60,60)) {
+		BUTTON.draw_TIMELINE.fast_forward.state = true;
+		BUTTON.draw_TIMELINE.fast_forward.draw();
+	}
+	
+});
+
+document.addEventListener('mouseup', function(event) {
+	let mouseX = event.clientX;
+	let mouseY = event.clientY;
+	function button_frame(x0,y0,dx,dy) {
+		if (mouseX >= x0 && mouseX <= (x0+dx) && mouseY >= y0 && mouseY <= (y0+dy)) {return true;}
+		else {return false;}
+	}
+	
+	// Rewind
+	if (button_frame(BUTTON.draw_TIMELINE.rewind.X,BUTTON.draw_TIMELINE.rewind.Y,60,60)) {
+		BUTTON.draw_TIMELINE.rewind.state = false;
+		BUTTON.draw_TIMELINE.rewind.draw();
+		if (TIME.dT == 0) {TIME.dT_memory *= 0.8;}
+		else {TIME.dT *= 0.8;}
+		
+	}
+	
+	// Play && Pause
+	if (button_frame(BUTTON.draw_TIMELINE.play_pause.X, BUTTON.draw_TIMELINE.play_pause.Y, 60, 60)) {
+		BUTTON.draw_TIMELINE.play_pause.state = false;
+		BUTTON.draw_TIMELINE.play_pause.switch();
+		BUTTON.draw_TIMELINE.play_pause.draw();
+		
+	}
+	
+	// Stop
+	if (button_frame(BUTTON.draw_TIMELINE.stop.X,BUTTON.draw_TIMELINE.stop.Y,60,60)) {
+		BUTTON.draw_TIMELINE.stop.state = false;
+		BUTTON.draw_TIMELINE.stop.draw();
 		RUNNING = false;
-		TIME.dT = - TIME.value;
 		context_ORBIT.clearRect(0,0,WIDTH,HEIGHT);
+		TIME.dT = - TIME.value;
 		draw();
-		TIME.dT = 3600;
 		RUNNING = true;
+		TIME.dT = 0;
 		draw();
-		BUTTON.draw_TIMELINE.switch();
+		if (BUTTON.draw_TIMELINE.play_pause.pause) {
+			BUTTON.draw_TIMELINE.play_pause.switch();
+			BUTTON.draw_TIMELINE.play_pause.draw();
+		}
+		TIME.dT_memory = 3600;
 	}
-	if (button_frame(WIDTH/2+80,HEIGHT-100,80,80)) {TIME.dT*=1.25;}
-	if (button_frame(WIDTH/2-120,HEIGHT-140,40,40)) {
-		FOCUS.num -=1;
-		if (FOCUS.num < 0) { FOCUS.num = DATA.length - 1;}
-		FOCUS.change(DATA[FOCUS.num]);
-	}
-	if (button_frame(WIDTH/2+80,HEIGHT-140,40,40)) {
-		FOCUS.num +=1;
-		if (FOCUS.num == DATA.length) { FOCUS.num = 0;}
-		FOCUS.change(DATA[FOCUS.num]);
+	
+	// Fast forward
+	if (button_frame(BUTTON.draw_TIMELINE.fast_forward.X,BUTTON.draw_TIMELINE.fast_forward.Y,60,60)) {
+		BUTTON.draw_TIMELINE.fast_forward.state = false;
+		BUTTON.draw_TIMELINE.fast_forward.draw();
+		if (TIME.dT == 0) {TIME.dT_memory *= 1.25;}
+		else {TIME.dT *= 1.25;}
 	}
 });
