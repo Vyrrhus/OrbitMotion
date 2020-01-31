9e2f459a8fe3305bae95924b527c1f64033238b1
Vyrrhus <baptistedesoubrie@gmail.com>
2020-01-31 17:15:15 +0100

    Fix BODIES declaration
    const declarations are all made in bodies.js
    Declaring bodies in script files mustn't be done as it will cause issues if the script is loaded multiple times.

--- src/data/scripts/sat_demo.js
+++ src/data/scripts/sat_demo.js
@@ -12,14 +12,6 @@ _SCRIPT.add_body(BODIES.EARTH, 		{x:-2.565776021787787E+07, 	y:1.459776792940268
 //_SCRIPT.add_body(SATELLITES.ECLIPTIC, {x:7371.009, y:0, z:0, vx:0, vy:7.35358372172849, vz:0, reference: BODIES.EARTH});
  
 _SCRIPT.add_body(BODIES.MOON, 		{x:-2.594384028858606E+07, 	y:1.457191985018245E+08, 	z:1.877793847522885E+04, 	vx:-2.920041610288576E+01, 	vy:-6.056705066205493E+00, 	vz:-1.966943187050774E-02});
-const SATs = {
-                _1: new Body('SAT 1', '#FF0000', 0, 0),
-                _2: new Body('SAT 2', '#FF5500', 0, 0),
-                _3: new Body('SOI TEST', '#55FF99', 0, 0),
-                _4: new Body('CRASH TEST', '#5555FF', 0, 0),
-                _5: new Body('SAT 5', '#FF9900', 0, 0),
-                _6: new Body('SAT 6', '#FF0099', 0, 0)
-             }
               
 //_SCRIPT.add_body(SATs._1,{i: 5.27, W: 116, e:0.5, a: BODIES.EARTH.radius + 230000, w:0, v:270, body:BODIES.EARTH});
 //_SCRIPT.add_body(SATs._2,{i: 5.27, W: 116, e:0.5, a: BODIES.EARTH.radius + 230000, w:25, v:270, body:BODIES.EARTH});
@@ -35,9 +27,6 @@ const SATs = {
 //_SCRIPT.add_body(SATs._4,{i: 5.330838730096627, W: 121.86930112466496, e:1.003350862472642, a: -500905.47975987464, w:221.22688733310392, v:211.4800831341708, body:BODIES.MOON});
 //_SCRIPT.add_body(SATs._3,{i: 5.330838730096627, W: 121.86930112466496, e:0.9, a: BODIES.MOON.radius + 50000, w:221.22688733310392, v:231.4800831341708, body:BODIES.MOON});
 
-// Inclined SATS crossing MOON's orbit
-const ENCOUNTER = {
-    a: new Body('CIRCULAR INCLINED', '#00FF99', 0, 0)
-}
+
 
 _SCRIPT.add_body(ENCOUNTER.a, {i: 30, W: 0, e: 0.8039626608269475, a: 214244, w: 0, v: 0, body:BODIES.EARTH})
