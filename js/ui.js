67512ba2706a0b7aedc92ec4cf1377de56f1a77f
Vyrrhus <baptistedesoubrie@gmail.com>
2020-02-01 12:39:19 +0100

    Fix slide() and fade() functions
    + add clamp() (might be added to src/utils.js ?) to set limits for opacity and margin and avoid issues.

--- js/ui.js
+++ js/ui.js
@@ -12,73 +12,63 @@ const helpSlideTime     = 250;
 
 // FUNCTIONS
 
-function fadeIn(el, time) {
-    el.style.opacity = 0;
-    el.style.display = "block";
-    
-    let title = el.querySelectorAll("h1,h2,h3,h4,h5");
-    for (let i=0 ; i < title.length ; i++) {
-        title[i].style.marginLeft = helpMarginLeft.toString() + "px";
-    }
-    
-    let last = +new Date();
+function clamp(a,b,c){ 
+    return ( (a<b) ? b : ( (a>c) ? c : a) ) 
+}
+
+function fadeIn(el, t0, dT) {
+    el.style.visibility = 'visible';
+    t0 += +new Date();
     let fade = function() {
-        el.style.opacity = +el.style.opacity + (new Date() - last) / time;
-        last = +new Date();
-        
+        el.style.opacity = clamp((+new Date() - t0) / dT, 0, 1);
         if (el.style.opacity < 1) {
             (window.requestAnimationFrame && requestAnimationFrame(fade)) || setTimeout(fade,16);
         }
-    };
-    let slide = function(el, delay, time, margin) {
-        
-        let begin = +new Date();
-        let tick = function() {
-            if (new Date() - begin > delay) {
-                let value = Math.round(margin * (1 - (new Date() - begin - delay) / time))
-                el.style.marginLeft = value.toString() + "px";
-                
-                if (value > 0) {
-                    (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick,16);
-                }
-            } else {
-                (window.requestAnimationFrame && requestAnimationFrame(tick)) || setTimeout(tick,16);
-            }
-        }
-        tick();
     }
     
     fade();
-    for (let i=0 ; i < title.length ; i++) {
-        slide(title[i], helpSlideDelay + i * helpSlideLag, helpSlideTime, helpMarginLeft);
-    }
 }
-
-function fadeOut(el, time) {
-    let last = +new Date();
+    
+function fadeOut(el, t0, dT) {
+    t0 += +new Date()
     let fade = function() {
-        el.style.opacity = +el.style.opacity - (new Date() - last) / time;
-        last = +new Date();
-        
+        el.style.opacity = clamp(1 - (+new Date() - t0) / dT, 0, 1);
         if (el.style.opacity > 0) {
             (window.requestAnimationFrame && requestAnimationFrame(fade)) || setTimeout(fade,16);
         } else {
-            let title = el.querySelectorAll("h1,h2,h3,h4,h5");
-            el.style.display = "none";
+            el.style.visibility = "hidden";
         }
     }
     
     fade();
 }
 
+function slideLeft(el, t0, dT, margin_0, margin_1) {
+    t0 += +new Date();
+    let slide = function() {
+        let marginLeft      = clamp(Math.round((margin_1 - margin_0) / dT * (+new Date() - t0) + margin_0), Math.min(margin_0, margin_1), Math.max(margin_0,margin_1));
+        el.style.marginLeft = marginLeft.toString() + "px";
+        if (el.style.marginLeft !== margin_1) {
+            (window.requestAnimationFrame && requestAnimationFrame(slide)) || setTimeout(slide,16);
+        }
+    }
+    
+    slide();
+}
+
 function showHelp() {
-    fadeIn(helpIn,      500);
-    fadeIn(helpOut,     200);
+    fadeIn(helpIn,  0,  500);
+    fadeIn(helpOut, 0,  200);
+    let title = helpIn.querySelectorAll("h1,h2,h3,h4,h5");
+    for (let i=0 ; i < title.length ; i++) {
+        title[i].style.marginLeft = helpMarginLeft.toString() + "px";
+        slideLeft(title[i], helpSlideDelay + helpSlideLag * i, helpSlideTime, helpMarginLeft, 0);
+    }
 }
 
 function hideHelp() {
-    fadeOut(helpIn,     200);
-    fadeOut(helpOut,    500);
+    fadeOut(helpIn,     0,  200);
+    fadeOut(helpOut,    0,  500);
 }
 
 function loadScenario(name) {
