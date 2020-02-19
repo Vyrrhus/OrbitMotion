4f6dfb7eac470a51ae6bfc931020bd9c830291aa
Vyrrhus <baptistedesoubrie@gmail.com>
2020-02-19 18:05:36 +0100

    Rework Touch Events
    + swipe on single touch for rotations

--- src/events/event.js
+++ src/events/event.js
@@ -185,11 +185,110 @@ document.addEventListener('click', function(event) {
 })
 
 // TOUCH EVENTS (mobile purpose)
+var CACHE_TOUCHES = {
+	list: new Array(),
+	prevTouches: new Array(),
+	minDist: 10,
+	length: 0,
+	center: {x: 0, y: 0},
+	add: function(touch) {
+		this.list.push(touch);
+		this.prevTouches.push(touch);
+		this.length += 1;
+		if (this.length === 2) {
+			this.center = this.getCenter(this.list[0], this.list[1]);
+		}
+	},
+	remove: function(touch) {
+		for (let i = 0 ; i < this.length ; i++) {
+			if (this.list[i].identifier === touch.identifier) {
+				this.list.splice(i,1);
+				this.prevTouches.splice(i,1);
+				this.length -= 1;
+				if (this.length === 2) {
+					this.center = this.getCenter(this.list[0], this.list[1]);
+				}
+				break;
+			}
+		}
+	},
+	getCenter: function(touchA, touchB) {
+		return {x: Math.floor((touchA.clientX + touchB.clientX) / 2),
+			    y: Math.floor((touchA.clientY + touchB.clientY) / 2)}
+	},
+	swipeDirection: function(curTouch) {
+		// Should be called if and only if length === 1
+		let center		= this.getCenter(curTouch, this.prevTouches[0]);
+		let swipeAngle	= curTouch.angle(center.x, center.y);
+		let swipeDist	= curTouch.radius(center.x, center.y) * 2;
+		if (swipeDist > this.minDist) {
+			this.prevTouches[0] = curTouch;
+			return {angle: swipeAngle, radius: swipeDist}
+		}
+		return null
+	}
+};
+
+Touch.prototype.radius	= function(centerX, centerY) {
+	return Math.hypot(this.clientX - centerX, centerY - this.clientY)
+}
+Touch.prototype.angle	= function (centerX, centerY) {
+	let hypot = this.radius(centerX, centerY);
+	if (hypot === 0) {
+		return 0
+	} else {
+		return quadrant((this.clientX - centerX) / hypot, (centerY - this.clientY) / hypot)
+	}
+}
+
 var PREV_DIFF	= -1;
+document.ontouchstart	= touchstart_handler;
+document.ontouchend		= touchend_handler;
+document.ontouchcancel	= touchend_handler;
 document.ontouchmove	= touchmove_handler;
 
+function touchstart_handler(ev) {
+	let touchList = ev.changedTouches;
+	for (let i = 0 ; i < touchList.length ; i++) {
+		CACHE_TOUCHES.add(ev.changedTouches[i]);
+	}
+}
+function touchend_handler(ev) {
+	let touchList = ev.changedTouches;
+	for (let i = 0 ; i < touchList.length ; i++) {
+		CACHE_TOUCHES.remove(ev.changedTouches[i]);
+	}
+}
 function touchmove_handler(ev) {
-	if (ev.changedTouches.length === 2) {
+	/*
+		Besoin de gérer la position des Touch avec un .identifier commun à l'instant précédent.
+		On pourrait imaginer définir une distance minimale entre les deux Touch pour considérer le move valide ? Pas énorme tho, quelques pixels
+		Si coefficient directeur globalement constant mais que la distance augmente / diminue ==> zoom
+		Si coefficient directeur évolue ==> rotation
+		Si distance trop faible ==> osef, on update pas le CACHE_TOUCH, un dict dont les entrées seront les identifier des Touch ? 
+		Un objet où l'on puisse ajouter des "familles" de Touch qui partagent un id commun, les comparer en terme de distance ou avec une autre famille pour obtenir le coefficient directeur
+		Nécessité peut-être de jongler entre screenX et clientX
+		Besoin de faire un site web qui permettent de tester toutes les configs possibles (github.io) : rotate, zoom in, zoom out, vitesse dudit zoom, distance minimale entre les deux, ellipse, etc.
+		
+		Besoin ensuite de créer de quoi bloquer les rotations / zoom sur mobile et de switch de mode pour afficher les trajectoires
+		Besoin également d'ajouter des boutons pour accélérer / réduire le temps
+		Réfléchir aux propriétés du zoom via le delta pour éventuellement l'améliorer
+		HELP ME spécial mobile ? Pour la partie contrôles?
+	*/
+	if (CACHE_TOUCHES.length === 1) {
+		// Rotation around a given vector
+		let touch	= ev.changedTouches[0];
+		let swipe	= CACHE_TOUCHES.swipeDirection(touch);
+		if (swipe !== null) {
+			let direction		= vect3.exp(swipe.angle, 1);
+			let vectRotation	= vect3.cross(PLANE.z, direction);
+			PLANE.x = quat.rotate(PLANE.x, 0.1 * swipe.radius, vectRotation);
+			PLANE.y = quat.rotate(PLANE.y, 0.1 * swipe.radius, vectRotation);
+			PLANE.z = vect3.cross(PLANE.x, PLANE.y);
+		}
+	}
+	if (CACHE_TOUCHES.length === 2) {
+		// Zoom inOut + rotate around K if circle
 		let touchList	= ev.changedTouches;
 		let touchA		= touchList[0];
 		let touchB		= touchList[1];
