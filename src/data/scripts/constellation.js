fd1fbfcdca9a412aa5fe8884c15ac7768c7a55d2
Vyrrhus <baptistedesoubrie@gmail.com>
2020-02-20 13:55:59 +0100

    Walkfer Delta constellation
    Set a proper way to define a mega-constellation

--- src/data/scripts/constellation.js
+++ src/data/scripts/constellation.js
@@ -1,25 +1,51 @@
 // CONSTELLATION
-var _SCRIPT = new Script("Constellation", {dT: 100, scale: 140, epoch:new Date(2019,0,1), plane:{x: CONST_I, y: CONST_J}});
+var _SCRIPT = new Script("Walker Delta", {dT: 10, scale: 50, epoch:new Date(2019,0,1), plane:{x: I, y: J}});
 _SCRIPT.add_body(BODIES.EARTH, 		{x:-2.565776021787787E+07, 	y:1.459776792940268E+08, 	z:-1.550401880809665E+04, 	vx:-2.984330712865031E+01, 	vy:-5.272844547694333E+00, 	vz:9.257075114927904E-04});
-//_SCRIPT.add_body(SATELLITES.MOLNIYA, 	{x:4.552697954638234e-13, y:-410.27021572905585, z:-7423.792007086785, vx:9.602465253717867, vy:3.2444837841682955e-17, vz:5.870855806880708e-16, reference: BODIES.EARTH});
-//_SCRIPT.add_body(SATELLITES.GEO, 		{x:38679.025673208154, y:0, z:16767.420218906234, vx:0, vy:3.0748760710235925, vz:0, reference: BODIES.EARTH});
 
-var nb_plans = 3;
-var nb_sat = 3;
-
-for (var i = 0 ; i < nb_plans ; i++) {
-	for (var j = 0 ; j < nb_sat ; j++) {
-		_SCRIPT.add_body(new Body(`GEO${i+1}.${j+1}`, 'white', 5000, 0),
-						 {i: 83, W: i/nb_plans * 360, e: 0, a: BODIES.EARTH.radius + 42000, w: 0, v: j/nb_sat*360, body: BODIES.EARTH});
-	}
+var WALKER_DELTA = {
+	/*	t:	number of satellites needed to satisfy the mission requirements.
+		P:	number of orbital planes that provide maximum coverage.
+		dW: ascending node W of p planes are equally distributed around the equator at intervals of dW = 360 / P.
+		S:	number of satellites per plane, S = t / P.
+		dv:	in each p plane, S satellites are equally distributed at interval dv = 360 / S
+		f:	spacing angular between satellites in adjacent planes is df = 360*f/t with f an integer between 0 and (P - 1)
+		i:	inclination of all P planes
+	*/
+	t: 150,
+	P: 30,
+	i: 60,
+	f: 8,
+	h: 550,
+	pxRadius: 3,
+	init: function(script) {
+		// WALKER DELTA DESIGN
+		this.dW = 360 / this.P;
+		this.S	= this.t / this.P;
+		this.dv	= 360 / this.S;
+		this.df	= 360 * this.f / this.t;
+		
+		// ADD BODIES
+		let k = 1;
+		for (let i = 0 ; i < this.P ; i++) {
+			for (let j = 0 ; j < this.S ; j++) {
+				let color	= 'white';
+				if (k === 1) {
+					color = 'red';
+				}
+				let body	= new Body('SAT-' + k,color,5000,0);
+				let options	= {i: this.i,
+							   W: i * this.dW,
+							   e: 0,
+							   a: BODIES.EARTH.radius + this.h,
+							   w: 0,
+							   v: j * this.dv + i * this.df,
+							   body: BODIES.EARTH,
+							   minRadius: this.pxRadius};
+				script.add_body(body, options)
+				k += 1;
+			}
+		}
+	},
 }
 
-//var nb_plans = 15;
-//var nb_sat   = 1;
-//
-//for (var i = 0 ; i < nb_plans ; i++) {
-//	for (var j = 0 ; j < nb_sat ; j++) {
-//		_SCRIPT.add_body(new Body(`POL${i+1}.${j+1}`, 'white', 5000, 0),
-//						 {i: 80, W: i/nb_plans * 360, e: 0, a: BODIES.EARTH.radius + 22000, w: 0, v: 90+j/nb_sat*360, body: BODIES.EARTH});
-//	}
-//}
+WALKER_DELTA.init(_SCRIPT);
